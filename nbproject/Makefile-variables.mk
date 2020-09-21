3338cfc8999ae5f2f506ad0ff2a1455d8963a21b
a.konovalov <a.konovalov@erlyvideo.org>
2020-09-21 22:49:23 +0300

    initial commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=cutests
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/cutests
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=cutests.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/cutests.tar
+# Release configuration
+CND_PLATFORM_Release=CUDA-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/CUDA-Linux
+CND_ARTIFACT_NAME_Release=cutests
+CND_ARTIFACT_PATH_Release=dist/Release/CUDA-Linux/cutests
+CND_PACKAGE_DIR_Release=dist/Release/CUDA-Linux/package
+CND_PACKAGE_NAME_Release=cutests.tar
+CND_PACKAGE_PATH_Release=dist/Release/CUDA-Linux/package/cutests.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
